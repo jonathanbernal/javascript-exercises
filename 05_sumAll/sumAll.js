982a8e5aa786e99ee8872c4ac4352e0ca0312f9d
Jonathan Bernal <jonathanbernal@users.noreply.github.com>
2022-12-22 18:41:54 -0500

    Implement sumAll() and cover all test cases

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+/**
+ *  This function returns the sum of all the numbers specified
+ *  in the range provided and includes the bounds as part of the result.
+ * @param {} low the lowest number to add from
+ * @param {*} high the highest number to add to
+ * @returns the sum of all the numbers specified by low and high.
+ */
+const sumAll = function(low, high) {
+    // Sanitize bounds
+    if( typeof(low) !== 'number' || typeof(high) !== 'number')
+      return 'ERROR';
+    else if(low < 0 || high < 0)
+      return 'ERROR';
 
+    let lowBound = Math.min(low, high);
+    let highBound = Math.max(low, high);
+    let accumulator = 0;
+
+    for(let i = lowBound; i <= highBound; i++){
+        accumulator += i;
+    }
+    
+    return accumulator;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
