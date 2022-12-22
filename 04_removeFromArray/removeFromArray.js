488410e29a67ebbe830767b75c4ca97da7739428
Jonathan Bernal <jonathanbernal@users.noreply.github.com>
2022-12-22 17:12:39 -0500

    Implement removeFromArray and pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(originalArray, ...args) {
+    let argsToRemove = [...args];
+    let currArray = originalArray;
+    
+    for(let i = 0; i < argsToRemove.length; i++){
+        currArray = currArray.filter(item => item !== argsToRemove[i]);
+    }
 
-};
+    return currArray;
+}
+
+removeFromArray([1, 3, 6, 9, 12, 15, 11, 18], 
+    1, 2, 3, 4, 5, 11, 12);
 
 // Do not edit below this line
 module.exports = removeFromArray;
