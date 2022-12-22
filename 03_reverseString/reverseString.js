9b5bc6a9a760343d94ee27865b01002d6ac4b693
Jonathan Bernal <jonathanbernal@users.noreply.github.com>
2022-12-22 15:14:52 -0500

    Implement reverseString() and test against suite

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(theString) {
+    return theString.split('').reverse().join('');
 };
+
+reverseString('Hello');
 
 // Do not edit below this line
 module.exports = reverseString;
