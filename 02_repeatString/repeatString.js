bdb2ab8187ef14ebd4a454d0c46a7db62dc13177
Jonathan Bernal <jonathanbernal@users.noreply.github.com>
2022-12-22 13:18:21 -0500

    Implement repeatString() and test it against suite

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(stringtoRepeat, times) {
+ if(times < 0) return 'ERROR';
+ 
+ let outputString = '';
+ for(let i = 0; i < times; i++)
+    outputString += stringtoRepeat;
+  return outputString;
 };
+
+console.log(repeatString('hey', 3));
 
 // Do not edit below this line
 module.exports = repeatString;
