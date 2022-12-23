c1e598390b9bb71280e9ec5344242f273036e981
Jonathan Bernal <jonathanbernal@users.noreply.github.com>
2022-12-23 02:06:49 -0500

    implement leapYears() and tempConversion() funcs

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(testYear) {
+    if (testYear % 4 === 0 && testYear % 100 !== 0)
+        return true;
+    else if (testYear % 400 === 0){
+        return true;
+    }
+    // If the conditions are not met, then the year
+    // is not a leap year.
+    return false;
 };
 
 // Do not edit below this line
